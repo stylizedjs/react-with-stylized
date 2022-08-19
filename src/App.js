d1f5e7cb916fd409ae01d18e6b861acf0a125ede
Gabriel Marinho <g.marinho@accenture.com>
2022-08-18 23:34:34 -0300

    adds Stylized button as example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/StlzdButton';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <stlzd-button>Click me</stlzd-button>
     </div>
   );
 }
