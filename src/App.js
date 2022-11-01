51d87c92364b5bd4b3f70dcbcdb63ce395078747
Gabriel Marinho <gdcmarinho@gmail.com>
2022-10-31 21:45:10 -0300

    updates Stylized button import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/StlzdButton';
+import '../node_modules/stylizedjs/dist/components/button/button.js';
 
 function App() {
   return (
